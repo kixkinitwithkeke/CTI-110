cd6d26ee7c666d5a23ea8a98325f5cf3241cb43f
kixkinitwithkeke <146122168+kixkinitwithkeke@users.noreply.github.com>
2023-09-26 14:19:34 -0400

    Add files via upload

---
+++ P1HW1_JamesKeora.py
@@ -0,0 +1,12 @@
+#Keora James
+#9/26/2023
+#Input,output,expression
+
+num1=int(input("Enter a number: "))
+print("You entered 4",num1)
+print(num1,"squared is",num1**2)
+print("And ",num1,"cubed is",num1**3)
+
+num2=int(input("Enter another number: "))
+print(num1, "+", num2,"=",num1+num2)
+print(num1,"*",num2,"=",num1*num2)
